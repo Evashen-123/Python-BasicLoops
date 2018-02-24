3e2f6f3d8ce34f001dd3d3e7551e2868580a07ea
Evashen-123 <36735944+Evashen-123@users.noreply.github.com>
2018-02-24 12:54:33 +0200

    Loops
    Basic concept and learning of for loop and while in Python

---
+++ WhileLoop.py
@@ -0,0 +1,10 @@
+num=1
+sum1=0
+print("enter number to add")
+print("Enter 0 to exit")
+while num!=0:
+    print("sum:",sum1)
+    num=float(input("enter number:"))
+    sum1+=num
+
+print("sum is :" , sum1)
---
+++ forLoop.py
@@ -0,0 +1,4 @@
+myList=[3,4,5,6,7,8,9,23,76,32,55]
+for num in myList :
+    print(num)
+input("enter to exit")
